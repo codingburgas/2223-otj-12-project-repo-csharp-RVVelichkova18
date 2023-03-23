b0800632e3cf45c9cd7c972926c5eeda8418e01f
RVVelichkova18 <RVVelichkova18@codingburgas.bg>
2023-03-23 13:42:30 +0200

    Transfer project solution

---
+++ ForestrySystem/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using MessagePack;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+
+namespace ForestrySystem.Models
+{
+    public class AppUser : IdentityUser
+    {
+
+        public string firstName { get; set; }
+      
+        public string lastName { get; set; }
+    }
+}
---
+++ ForestrySystem/Models/CategoryOfTimber.cs
@@ -0,0 +1,18 @@
+﻿using ForestrySystem.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForestrySystem.Models
+{
+    public class CategoryOfTimber
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Category Name")]
+        public TypeOfTimberEnum CategoryName { get; set; }
+        [DisplayName("Amount for Logging")]
+        public float AmountForLogging { get; set; }
+        [DisplayName("Year of Logging")]
+        public DateTime YearOfLogging { get; set; }
+    }
+}
