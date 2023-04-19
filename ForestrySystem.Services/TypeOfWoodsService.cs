3e0ea5546e4518143ca031a32484ddf6e65581d1
RVVelichkova18 <RVVelichkova18@codingburgas.bg>
2023-04-19 17:08:45 +0300

    Develop 3-layer structure and transfer data and logic into rigth places

---
+++ ForestrySystem.Services/TypeOfWoodsService.cs
@@ -0,0 +1,14 @@
+﻿using ForestrySystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestrySystem.Services
+{
+	public class TypeOfWoodsService
+	{
+		public TypeOfWood
+	}
+}
